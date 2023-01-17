82c5722cd90f9e5af0f34cbb9d906027bd2a13e7
Avocoder <andrii.otroshenko@gmail.com>
2023-01-17 11:31:38 +0200

    add: styles, column, card, new item components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { AddNewItem } from "./AddNewItem";
+import { Column } from "./Column";
+import { AppContainer } from "./styled";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <Column text="TODO:" />
+      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
+    </AppContainer>
   );
-}
-
-export default App;
+};
---
+++ src/Column.tsx
@@ -0,0 +1,17 @@
+import { Card } from "./Card";
+import { ColumnContainer, ColumnTitle } from "./styled";
+
+type ColumnProps = {
+  text: string;
+};
+
+export const Column = ({ text }: ColumnProps) => {
+  return (
+    <ColumnContainer>
+      <ColumnTitle>{text}</ColumnTitle>
+      <Card text="Generate app scaffold" />
+      <Card text="Learn TypeScript" />
+      <Card text="Begin to use static typing" />
+    </ColumnContainer>
+  );
+};
